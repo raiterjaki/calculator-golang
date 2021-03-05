1280d73963788726d0261c7b9e61064cf6bd7507
raiterjaki <45034043+raiterjaki@users.noreply.github.com>
2021-03-05 22:19:41 +0400

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,11 @@
   CHANGELOG [5th of March, 2021]:
     - Added rectangle struct
     - Added functions for rectangle such as area finder and perimeter finder
+
+  [5th of March 2021 (2nd addition today)]
+    - Added isosceles triangle struct
+    - Added functions for isosceles triangle such as area finder and perimeter finder
+    
   github.com/raiterjaki
   raiterjaki#0796
   */
@@ -33,6 +38,18 @@ func tanNum(a1 float64) float64 { return math.Tan(a1) }
 type rectangle struct{ height, length int }
 func findRecPerimeter(r rectangle) int { return r.height + r.height + r.length + r.length }
 func findRecArea(r rectangle) int { return r.height * r.length }
+type isosceles_triangle struct { A_side, B_side, C_side, height int }
+func findIsTrPerimeter(t isosceles_triangle) int {
+  if t.A_side != t.B_side {
+    fmt.Println("[error] your variables are wrong. there are 2 equal sides in isosceles triangle. if you have 2 equal numbers but mentioned them as c or b, you can modify this code or just rearrange them. Even thought it is wrong, we still solved it for you.")
+  } else if t.C_side < t.A_side | t.B_side {
+    fmt.Println("[error] base of an isosceles triangle can't be smaller than the a and b. But we will still solve it for you.")
+  }
+  return t.A_side + t.B_side + t.C_side
+}
+func findIsTrArea(t isosceles_triangle) int {
+  return (t.C_side * t.height) / 2 // C_side is the base.
+}
 
 //math funcs (end)a
 
@@ -44,4 +61,6 @@ func main() {
     fmt.Println("Your answer is ", sinNum(50)) // this will provide you your sine result with exact number.
     fmt.Println("Perimeter of rectangle with height: 5 length: 7 is ", findRecPerimeter(rectangle{5, 7}))
     fmt.Println("Area of rectangle with height: 5 length: 7 is ", findRecArea(rectangle{5, 7}))
+    fmt.Println("Perimeter of triange with a: 3, b: 3, c: 5 is ", findIsTrPerimeter(isosceles_triangle{3, 3, 5, 0})) // 0 = height
+    fmt.Println("Area of a triangle with a: 5, b: 5, c: 7 is ", findIsTrArea(isosceles_triangle{0, 0, 7, 4})) //base 7, height 4
 }
